6d64a246297678c49c5fe4604a102ef0a8864980
SureshRam134 <ss9477157@gmail.com>
2026-06-02 00:19:25 +0530

    suresh:chatting

--- src/Components/Chatting.jsx
+++ src/Components/Chatting.jsx
@@ -91,7 +91,7 @@ const Chatting = ({ currentUser = {}, backToScreen }) => {
                     <input type="text" name="message" id="" placeholder='Send Message....'
                         value={message}
                         onChange={(e) => { setMessage(e.target.value) }}
-                        className='flex-3 py-2 md:py-3 px-5 bg-gray-500/30 rounded-md text-white outline-none text-base'
+                        className='flex-3 py-2 md:py-3 px-5 bg-gray-500/30 rounded-md  text-white outline-none text-base'
                     />
                     <button className='flex-1 py-2 md:py-3 bg-green-500/30 rounded-md text-white outline-none text-base' onClick={chatMessageFunction} type='button'>Send</button>
                 </div>
